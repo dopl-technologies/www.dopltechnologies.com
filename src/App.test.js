56c4a0591d29aea3e3fb80afdba4ec3f67c09dba
Ryan C James <rcjames1004@gmail.com>
2020-12-16 23:57:59 -0800

    Prepare to deploy to heroku (#2)

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders coming soon', () => {
+  render(<App />);
+  const comingSoon = screen.getByText(/Coming soon/i);
+  expect(comingSoon).toBeInTheDocument();
+});
