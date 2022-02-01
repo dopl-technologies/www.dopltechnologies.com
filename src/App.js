3627273d71eddc59a1cae4c963bb92659590b575
Ryan James <ryan@plutovr.com>
2022-01-31 21:32:17 -0800

    Lowercase h

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Home page coming soon
+          home page coming soon
         </p>
       </header>
     </div>
